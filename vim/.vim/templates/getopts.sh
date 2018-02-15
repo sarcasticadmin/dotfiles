74f5cfa30b8611650761cb0ad4775d73f02a7ed1
Robert James Hernandez <rob@sarcasticadmin.com>
2018-02-14 21:02:03 -0800

    Adding simple vim shell templates

---
+++ vim/.vim/templates/getopts.sh
@@ -0,0 +1,37 @@
+usage(){
+  cat << EOF
+usage: $(basename $0) [OPTIONS] ARGS
+
+Simple template of getopts
+
+OPTIONS:
+  -h      Show this message
+  -e      Echo first arg
+
+EXAMPLES:
+  To print out arg1:
+
+      $(basename $0) arg1
+
+EOF
+}
+
+while getopts "he:" OPTION
+do
+  case $OPTION in
+    e )
+      ARG1=$OPTARG
+      ;;
+    h )
+      usage
+      exit 0
+      ;;
+    \? )
+      usage
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+echo ${ARG1:-'notset'}
