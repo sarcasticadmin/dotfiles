09543f6fe0d7e83185477931c93f5b8b14272106
Robert James Hernandez <rob@sarcasticadmin.com>
2021-10-19 21:29:36 -0700

    VIM_PLUG_VER variable in makefile

--- _include/vim.mk
+++ _include/vim.mk
@@ -1,4 +1,6 @@
+VIM_PLUG_VER ?= fc2813ef4484c7a5c080021ceaa6d1f70390d920
+
 update-vim-plug:
 	curl -fLo ./vim/.vim/autoload/plug.vim --create-dirs \
-	  https://raw.githubusercontent.com/junegunn/vim-plug/fc2813ef4484c7a5c080021ceaa6d1f70390d920/plug.vim
+	  https://raw.githubusercontent.com/junegunn/vim-plug/$(VIM_PLUG_VER)/plug.vim
 
