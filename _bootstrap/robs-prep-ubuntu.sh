aaae9472fa23cb6239c9c8b3289bee1f29a1f650
Robert James Hernandez <rob@sarcasticadmin.com>
2022-03-07 20:47:05 -0800

    Adding gpg and scdaemon to bootsrap

--- _bootstrap/robs-prep-ubuntu.sh
+++ _bootstrap/robs-prep-ubuntu.sh
@@ -3,7 +3,8 @@
 # Bare minimuium that I need to get up and running if on Ubuntu
 # Leave the rest of nix
 sudo apt-get update
-sudo apt-get install stow tmux vim git curl xsel
+sudo apt-get install stow tmux vim git curl xsel \
+             gnupg2 gnupg-agent gnupg-curl scdaemon pcscd pinentry-tty # pgp card
 
 # Turn off ondemand CPU schedule that slows the system down
 sudo systemctl stop ondemand
