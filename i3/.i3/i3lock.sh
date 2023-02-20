6d358c766a38cd67ced5e9be3834fa7db41c24d0
Robert James Hernandez <rob@sarcasticadmin.com>
2023-02-20 17:15:33 +0000

    simple and fix i3lock w/dpms
    previous i3locking code was reenabling dpms since it was blanking the
    screen upon locking. Added a trap to make sure to zero out (effectively
    disabling dpms).
    
    i3lock.sh was writing unnecessary tmp files and blurring was slow. New
    changes make this much snappier and make the blurring more extreme.
    
    In the future it would probably be better to read in
    the existing settings of dpms and restore them after login.

--- i3/.i3/i3lock.sh
+++ i3/.i3/i3lock.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 8x8 /tmp/screen_locked2.png
-i3lock -e -f -i /tmp/screen_locked2.png
-xset dpms force off
-# Need to do something clever here
-#doas killall -u root -USR1 moused
+
+# check the value of DPMS: xset q
+
+# Revert all temporary behavior after logging back in
+revert() {
+  # Disable screen blank
+  xset s off
+  # "Disable" DPMS
+  # Its better to zero out the dpms values than disable dpms: xset -dpms
+  # Since its easy for other things to reenable it
+  # e.g.: xset dpms force off (force screen off)
+  xset dpms 0 0 0
+}
+
+# trigger revert for all process signals, mainly EXIT
+trap revert HUP INT TERM EXIT
+
+# Screenshot then fast blur the current screen
+scrot - | convert - -scale 10% -blur 0x2.5 -resize 1000% /tmp/i3lock.png
+# Temporary set vals for DPMS while locked (in seconds)
+# standby | suspend | off
+# off must be equal to or greater than standby and suspend
+xset dpms 0 0 600
+i3lock -n -e -f -i /tmp/i3lock.png
